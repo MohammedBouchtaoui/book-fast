76ff0cf3cb3421c71e5480bb99d27480e599b94c
MohammedBouchtaoui <mohammedbouchtaoui48@gmail.com>
2026-03-09 20:30:29 +0000

    Initial commit: Ajout du projet Spring Boot dans le monorepo

---
+++ book-fast/src/main/java/com/BouchtaouiMohammed/book_fast/BookFastApiApplication.java
@@ -0,0 +1,13 @@
+package com.BouchtaouiMohammed.book_fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFastApiApplication.class, args);
+	}
+
+}
---
+++ book-fast/src/test/java/com/BouchtaouiMohammed/book_fast/BookFastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BouchtaouiMohammed.book_fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
